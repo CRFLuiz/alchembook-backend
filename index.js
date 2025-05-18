0adbf791d7591a6853694a6f3e73b2ce8c9fe025
Ubuntu <ubuntu@ip-172-31-18-211.us-west-2.compute.internal>
2025-05-18 13:27:24 +0000

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let envFile = process.env.ENV ? `.env.${process.env.ENV.toLocaleLowerCase()}` : 
 if(envFile) require('dotenv').config({ path: envFile });
 
 const express = require('express');
+const cors = require('cors');
 
 const routes = require('./src/routes');
 
@@ -11,6 +12,9 @@ const PORT = process.env.PORT || 3000;
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static('public'));
+server.use(cors({
+  origin: ['http://localhost:8000', 'https://your-production-domain.com']
+}));
 server.use(routes);
 
 server.listen(PORT, err => console.log(err || `Server is running on port ${PORT}`));
